b1f91da5e698bf3c541b62f36c1e904b269541a8
Dilsher Balouch <dilsherb@gmail.com>
2023-01-16 19:40:46 +0300

    linters

--- script.js
+++ script.js
@@ -1,35 +1,34 @@
 const bookContainer = document.querySelector('.bookContainer');
-let books = JSON.parse(localStorage.getItem('books')) || [];
-books.forEach(book => {
-    bookContainer.innerHTML += `
-        <p>"${book.title}"</p>
-        <p>by ${book.author}</p>
-        <button class="removeBtn" id="${book.id}">Remove</button>
-        <hr>
-`;    
+const books = JSON.parse(localStorage.getItem('books')) || [];
+books.forEach((book) => {
+  bookContainer.innerHTML += `
+  <p>"${book.title}"</p>
+  <p>by ${book.author}</p>
+  <button class="removeBtn" id="${book.id}">Remove</button>
+  <hr>
+`;
 });
 const addBook = () => {
-    let formData = document.querySelectorAll('input');
-    const title = formData[0].value;
-    const author = formData[1].value;
-    const id = Date.now().toString();
-    books.push({title, author, id});
-    localStorage.setItem('books', JSON.stringify(books));
-}
+  const formData = document.querySelectorAll('input');
+  const title = formData[0].value;
+  const author = formData[1].value;
+  const id = Date.now().toString();
+  books.push({title, author, id});
+  localStorage.setItem('books', JSON.stringify(books));
+};
 
 document.querySelector('.addBtn').addEventListener('click', addBook);
 
 const removeBook = (id) => {
-    let filteredBooks = books.filter((book) => book.id !== id);
-    localStorage.setItem('books', JSON.stringify(filteredBooks));
-    location.reload();
-}
+  const filteredBooks = books.filter((book) => book.id !== id);
+  localStorage.setItem('books', JSON.stringify(filteredBooks));
+  location.reload();
+};
 // document.querySelector('removeBtn').addEventListener('click', removeBook);
-bookContainer.addEventListener("click", (e) => {
-    const clickedBtn = e.target.closest(".removeBtn");
-    if (!clickedBtn) return;
-  
-    const idToRemove = clickedBtn.id;
-  
-    removeBook(idToRemove);
-  });
+bookContainer.addEventListener('click', (e) => {
+  const clickedBtn = e.target.closest(".removeBtn");
+  if (!clickedBtn) return;
+  const idToRemove = clickedBtn.id;
+
+  removeBook(idToRemove);
+});
