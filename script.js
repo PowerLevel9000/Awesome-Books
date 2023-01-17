f151d0e10250311591be1e7affe57de233ea971a
PowerLevel9000 <116451144+PowerLevel9000@users.noreply.github.com>
2023-01-17 19:33:35 +0530

    add remove method

--- script.js
+++ script.js
@@ -1,80 +1,34 @@
+const bookContainer = document.querySelector('.bookContainer');
 class BookShelf {
   constructor() {
-    this.bookContainer = document.querySelector('.bookContainer');
-    this.books = JSON.parse(localStorage.getItem('books')) || [];
+    this.book = JSON.parse(localStorage.getItem('books')) || [];
   }
 
-  genHtml() {
-    
-    this.books.forEach((book) => {
-      bookContainer.innerHTML += `
-      <div class="innerContainer">
-        <p>"${book.title}" by ${book.author}</p>
-        <button class="removeBtn" id="${book.id}">Remove</button>
-      </div>
-    `;
-    });
+  addBook = () => {
+    const formData = document.querySelectorAll('input');
+    const title = formData[0].value;
+    const author = formData[1].value;
+    if (title.trim() === '' || author.trim() === '') return;
+    const id = Date.now().toString();
+    this.book.push({ title, author, id });
+    console.log(this.book)
+    localStorage.setItem('books', JSON.stringify(this.book));
+    window.location.reload();
+  };
 
-    const addBook = () => {
-      const formData = document.querySelectorAll('input');
-      const title = formData[0].value;
-      const author = formData[1].value;
-      if (title.trim() === '' || author.trim() === '') return;
-      const id = Date.now().toString();
-      this.books.push({ title, author, id });
-      localStorage.setItem('books', JSON.stringify(this.books));
-      console.log(this.books)
-      // window.location.reload();
-    };
-    document.querySelector('.addBtn').addEventListener('click', addBook);
-  }
-
-  // removeBook(id) {
-  //   const filteredBooks = books.filter((book) => book.id !== id);
-  //   localStorage.setItem('books', JSON.stringify(filteredBooks));
-  //   window.location.reload();
-  //   this.bookContainer.addEventListener('click', (e) => {
-  //     const clickedBtn = e.target.closest('.removeBtn');
-  //     if (!clickedBtn) return;
-  //     const idToRemove = clickedBtn.id;
-  //     removeBook(idToRemove);
-  //   });
-  // }
 
 }
+const g = new BookShelf()
+g.book.forEach((b) => {
+  bookContainer.innerHTML += `
+      <div class="innerContainer">
+        <p>"${b.title}" by ${b.author}</p>
+        <button class="removeBtn" id="${b.id}">Remove</button>
+      </div>
+    `;
+});
 
-// window.onload(BookShelf.genHtml)
-// const books = JSON.parse(localStorage.getItem('books')) || [];
-// books.forEach((book) => {
-//   bookContainer.innerHTML += `
-//   <div class="innerContainer">
-//     <p>"${book.title}" by ${book.author}</p>
-//     <button class="removeBtn" id="${book.id}">Remove</button>
-//   </div>
-// `;
-// });
 
-// const addBook = () => {
-//   const formData = document.querySelectorAll('input');
-//   const title = formData[0].value;
-//   const author = formData[1].value;
-//   if (title.trim() === '' || author.trim() === '') return;
-//   const id = Date.now().toString();
-//   books.push({ title, author, id });
-//   localStorage.setItem('books', JSON.stringify(books));
-//   window.location.reload();
-// };
+document.querySelector('.addBtn').addEventListener('click', g.addBook);
+console.log(g.book)
 
-// document.querySelector('.addBtn').addEventListener('click', addBook);
-
-// const removeBook = (id) => {
-//   const filteredBooks = books.filter((book) => book.id !== id);
-//   localStorage.setItem('books', JSON.stringify(filteredBooks));
-//   window.location.reload();
-// };
-// bookContainer.addEventListener('click', (e) => {
-//   const clickedBtn = e.target.closest('.removeBtn');
-//   if (!clickedBtn) return;
-//   const idToRemove = clickedBtn.id;
-//   removeBook(idToRemove);
-// });
