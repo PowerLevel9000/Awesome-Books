41f7701c9d35697c5524a96053da5a84a5fb47aa
Dilsher Balouch <dilsherb@gmail.com>
2023-01-18 13:52:02 +0300

    menu toggle functionality added.

--- script.js
+++ script.js
@@ -1,3 +1,28 @@
+const menuItems = document.querySelectorAll('li');
+const sections = document.querySelectorAll('section');
+function reset() {
+  sections.forEach((section) => {
+  section.style.display = 'none';
+  });
+};
+reset();
+sections[0].style.display = 'block';
+
+menuItems[0].addEventListener('click', () => {
+  reset();
+  sections[0].style.display = 'block';
+});
+
+menuItems[1].addEventListener('click', () => {
+  reset();
+  sections[1].style.display = 'block';
+});
+
+menuItems[2].addEventListener('click', () => {
+  reset();
+  sections[2].style.display = 'block';
+});
+
 const menuBtn = document.getElementById('burger-container');
 const navOverlay = document.getElementById('nav');
 const navList = document.getElementsByClassName('menu')[0];
