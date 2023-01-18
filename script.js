c1aa4832dfd5ab0ac96ad05ead0e13c416b893f4
PowerLevel9000 <116451144+PowerLevel9000@users.noreply.github.com>
2023-01-18 15:33:22 +0530

    added burger and overlay

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
+const menuBtn = document.getElementById('burger-container');
+const navOverlay = document.getElementById('nav');
+const navList = document.getElementsByClassName('menu')[0];
+const burgerBar = document.getElementsByClassName('hamRotate')[0];
+function overlay() {
+  navOverlay.classList.toggle('invisible');
+  burgerBar.classList.toggle('active');
+  navOverlay.style.animation = 'awesome-out 1s';
+}
+
+menuBtn.addEventListener('click', overlay);
+navList.addEventListener('click', overlay);
+
 class BookShelf {
   constructor() {
     this.book = JSON.parse(localStorage.getItem('books')) || [];
